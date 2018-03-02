0af16ba9753ab4e92e3f87dc73a2c26a2257f5c8
51115152017.2 <51115152017.2@51DE194266.senacrjedu.local>
2018-03-01 21:55:26 -0300

    primeiro comint

---
+++ OlaMundo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title> Helo Wold </title>
+    </head>
+    <body>
+        <h1> Minha primeira pagina em php com codigos basico "Ola Mundo " </h1>
+        <code>
+            <!-- para iniciarmos um bloco de codigos php usamos as tags a baixo -->
+            <?php
+                
+                # para declararmos uma variavel é nescesario usar o simbolo $
+                $hello = "ola mundo php";
+                
+                // comando utilizado para imprimir (saida) na tela
+                echo($hello);
+
+            /* detalhes importante ;
+
+                Em php sempre sera nescesario iniciar uma variavel com $.
+                para que o servidor entenda os codigos php os arquivos
+                devem ser salvos com a extensao .php.
+            
+            */
+            ?>
+        </code>
+    </body>
+</html>
+
